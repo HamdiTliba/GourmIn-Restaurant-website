0881ce95e910cd2fce66d355a558581c0dcb74b2
HamdiTliba <hamditliba.go@gmail.com>
2023-03-30 18:20:15 +0100

    add footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,12 +1,49 @@
 import React from "react";
-import { FiFacebook, Fitwitter, FiInstagram } from "react-icons/fi";
+import { FiFacebook, FiInstagram, FiTwitter } from "react-icons/fi";
 import { FooterOverlay, Newsletter } from "../../components";
+import { images } from "../../constants";
 import "./Footer.css";
 
 const Footer = () => (
-  <div className="app__footer section__padding">
+  <div className="app__footer section__padding flex__center" id="login">
     <FooterOverlay />
     <Newsletter />
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">9 W 55RD St, New York,NY 10019,USA</p>
+        <p className="p__opensans">+1 212-333-1111</p>
+        <p className="p__opensans">+1 212-333-2222</p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="gericht" className="spoon_img" />
+        <p className="p__opensans">
+          "The best way to find yourself is to lose yourself in the service of
+          others"
+        </p>
+        <img
+          src={images.spoon}
+          alt="spoon"
+          className="spoon__img"
+          style={{ marginTop: "15px" }}
+        />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiInstagram />
+          <FiTwitter />
+        </div>
+      </div>
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">Saturday - Sunday :</p>
+        <p className="p__opensans">07:00 am - 11:00 am</p>
+      </div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">2023 GourmIn. All Rights reserved. </p>
+    </div>
   </div>
 );
 
